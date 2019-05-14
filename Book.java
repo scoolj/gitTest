3901c2410b8807ec21108f1b408e17026ba2373e
unknown <scoolj99@gmail.com>
2019-05-14 15:03:38 +0100

    first commit

---
+++ Book.java
@@ -0,0 +1,12 @@
+// Scoolj , oluwajuwonlo
+// java
+// gitTest
+
+public class Book{
+    private String title;
+    private String author;
+    private int Pages;
+    private String color;
+    private boolean isHardCover; 
+    private String publisher;      
+}
